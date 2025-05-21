481e5024eb617138ce4613604da02ecd3c7179a7
Laura <rw21062006@gmail.com>
2025-05-21 10:57:43 +0200

    implement file getter

---
+++ src/routes/[filename]/+server.ts
@@ -0,0 +1,28 @@
+import { existsSync, readFileSync } from "fs";
+import { fileTypeFromFile } from "file-type";
+import type { RequestHandler } from "@sveltejs/kit";
+
+export const GET: RequestHandler = async ({ params }) => {
+    const { filename } = params;
+
+    const path = `./files/${filename}`;
+    if (existsSync(path)) {
+        const mimeTypeReader = fileTypeFromFile(path);
+        const file = readFileSync(path);
+        const mimeType = (await mimeTypeReader)?.mime;
+
+        return new Response(file, {
+            status: 200,
+            headers: {
+                "Content-Type": mimeType || "application/octet-stream",
+            },
+        });
+    } else {
+        return new Response("File not found", {
+            status: 404,
+            headers: {
+                "Content-Type": "text/plain",
+            },
+        });
+    }
+}
