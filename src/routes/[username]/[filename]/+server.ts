44ecb6d92f1f7bfccc2829d3c9c3468840619be8
Laura <rw21062006@gmail.com>
2025-05-23 13:04:54 +0200

    use file ext as mime type fallback

--- src/routes/[username]/[filename]/+server.ts
+++ src/routes/[username]/[filename]/+server.ts
@@ -1,6 +1,8 @@
 import { existsSync, readFileSync } from "fs";
+import { readFile } from "fs/promises";
+import mime from "mime";
 import { fileTypeFromFile } from "file-type";
-import type { RequestHandler } from "@sveltejs/kit";
+import type { RequestHandler } from "./$types";
 
 export const GET: RequestHandler = async ({ params }) => {
     const { username, filename } = params;
@@ -8,13 +10,18 @@ export const GET: RequestHandler = async ({ params }) => {
     const path = `./files/${username}/${filename}`;
     if (existsSync(path)) {
         const mimeTypeReader = fileTypeFromFile(path);
-        const file = readFileSync(path);
-        const mimeType = (await mimeTypeReader)?.mime;
+        const fileReader = readFile(path);
+        let mimeType = (await mimeTypeReader)?.mime;
+        // file extension fallback
+        if (!mimeType) {
+            mimeType = mime.getType(filename) || "application/octet-stream";
+        }
+        const file = await fileReader;
 
         return new Response(file, {
             status: 200,
             headers: {
-                "Content-Type": mimeType || "application/octet-stream",
+                "Content-Type": mimeType,
             },
         });
     } else {
